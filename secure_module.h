c1f965ac02eee7d39e69600868842e0483f7871f
doyourcat <138800654+doyourcat@users.noreply.github.com>
2025-11-16 20:26:21 +0900

    Delete secure_module.h

--- secure_module.h
+++
@@ -1,7 +0,0 @@
-struct SecurePointer;
-
-struct SecurePointer* secure_create(void* real_data, void* decoy_data);
-
-void secure_destroy(struct SecurePointer* sp);
-
-void* secure_read(struct Secure_Pointer* sp);
